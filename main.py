ec7aa8f45ea211377b8314cf77bb97f4934bc5c4
mmsolovev <mmsolovevmail@gmail.com>
2022-06-22 21:53:05 +0300

    add: validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from schemas import Book
 
 
@@ -30,3 +30,8 @@ def create_book(item: Book):
 @app.get('/book')
 def get_book(q: List[str] = Query("test", description="Search book", deprecated=True)):
     return q
+
+
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+    return {"pk": pk, "pages": pages}
