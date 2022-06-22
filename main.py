ecfbed571d75828ee6c43aae5c3dcd381beb866a
mmsolovev <mmsolovevmail@gmail.com>
2022-06-22 22:55:25 +0300

    add: author schema

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from typing import List
 
-from fastapi import FastAPI, Query, Path
-from schemas import Book
-
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 app = FastAPI()
 
@@ -23,8 +22,13 @@ def home():
 
 
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    return {"item": item, "author": author, "quantity": quantity}
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {"author": author}
 
 
 @app.get('/book')
