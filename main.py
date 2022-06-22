e76583332d1e9e3120d3120dcc16e1e0a153f35c
mmsolovev <mmsolovevmail@gmail.com>
2022-06-22 22:07:30 +0300

    fix: structure

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from schemas import Joke
 app = FastAPI()
 
 
-@app.get('/')
-def get_joke(q: Joke):
+@app.post('/joke')
+def create_joke(item: Joke):
+    return item
+
+
+@app.get('/joke')
+def get_joke(q: str = 'test'):
     return q
+
+
+@app.get('/joke/{pk}')
+def get_single_joke(pk: int):
+    return {"pk": pk}
--- schemas.py
+++ schemas.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
+from datetime import date
 
 
 class Joke(BaseModel):
     id: int
     text: str
+    author: str
+    date: date
     rating: str
